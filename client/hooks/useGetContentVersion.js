414d4471e13959d4d393c5c34acb75fc6736005d
Antonios <lib3rat0r@gmail.com>
2026-04-06 15:13:19 +0300

    - update content at :30 or :00

--- client/hooks/useGetContentVersion.js
+++ client/hooks/useGetContentVersion.js
@@ -1,12 +1,29 @@
+import { useEffect } from 'react';
 import useSWR from 'swr';
 import LocalApiService from '@/client/services/LocalApiService';
 
 function useGetContentVersion() {
-    const { data, isValidating, isLoading } = useSWR('content-version', () => LocalApiService.getContentVersion(), {
-        refreshInterval: 30000,
+    const { data, isValidating, isLoading, mutate } = useSWR('content-version', () => LocalApiService.getContentVersion(), {
         refreshWhenHidden: false,
         revalidateOnFocus: true,
     });
+
+    useEffect(() => {
+        let intervalId;
+
+        // Sync with :00 and :30 of the minute
+        const msUntilNextSync = 30000 - (Date.now() % 30000);
+
+        const timeoutId = setTimeout(() => {
+            mutate();
+            intervalId = setInterval(mutate, 30000);
+        }, msUntilNextSync);
+
+        return () => {
+            clearTimeout(timeoutId);
+            if (intervalId) clearInterval(intervalId);
+        };
+    }, [mutate]);
 
     return {
         version: data?.version,
